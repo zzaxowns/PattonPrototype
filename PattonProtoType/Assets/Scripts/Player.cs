4ff0f46c3d8b9eac51e5501e2863f0b349ca02eb
zzaxowns <46182207+zzaxowns@users.noreply.github.com>
2021-10-19 17:30:42 +0900

    움직이는 블록 위에서 플레이어같이 움직이게 만들기

--- PattonProtoType/Assets/Scripts/Player.cs
+++ PattonProtoType/Assets/Scripts/Player.cs
@@ -4,24 +4,22 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-	MoveBlock block = GameObject.Find("Block").GetComponent<MoveBlock>();
+	MoveBlock block;
 
     // Start is called before the first frame update
     void Start()
     {
-		
-    }
+		block = GameObject.Find("Block").GetComponent<MoveBlock>(); 
+	}
 
     // Update is called once per frame
     void Update()
     {
-		Debug.Log(block.getSpeed());
-		Debug.Log("HI");
 	}
 
 	private void OnTriggerStay(Collider other)
 	{
-		if (other.transform.CompareTag("MoveBlock"))
+		if (other.transform.tag == "MoveBlock")
 		{
 			float speed = block.getSpeed();
 			this.transform.Translate(speed * Time.deltaTime, 0.0f, 0.0f);
